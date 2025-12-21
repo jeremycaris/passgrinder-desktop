7bf3c84897ff4484bb7f1908a15015c581809414
Jeremy Caris <jeremycaris@pm.me>
2025-12-21 10:11:52 -0500

    Adjust main window height to improve layout consistency

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -23,7 +23,7 @@ class MainFlutterWindow: NSWindow {
     self.backgroundColor = NSColor.clear
     self.level = .floating
 
-    let desiredContentSize = NSSize(width: 460, height: 400)
+    let desiredContentSize = NSSize(width: 460, height: 380)
     self.setContentSize(desiredContentSize)
     self.minSize = desiredContentSize
     self.maxSize = desiredContentSize
